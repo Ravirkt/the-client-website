f629c51b3200a347125d30cb7da5c0a9bd3a1fa8
Ravirkt <r_tjikhoeri@live.com>
2024-09-26 15:51:20 +0200

    added the playlist pagen dropdown nav
    styling moet nog goed gedaan worden, principe werkt wel voor de nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-// JS
+
+
+
+function openmenu() {
+    const hammenu = document.querySelector('.test')
+    hammenu.style.display = 'flex';
+}
+
+function closemenu() {
+    const hammenu = document.querySelector('.ham-menu')
+    hammenu.style.display = 'none';
+}
+
+
+
