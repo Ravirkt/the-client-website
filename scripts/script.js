eaf4241185027e247d7aa973e0a13eeb082e0f1f
Ravirkt <r_tjikhoeri@live.com>
2024-10-12 19:16:33 +0200

    comments toegevoegd #20

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,19 @@ function closemenu() {
 }
 
 
+/* Pauze/play knop
+Voor deze functionaliteit heb ik de html video element een id gegeven en ook de svg met een anker element heb ik een id gegeven.
+Deze fungeren als de knop. Doormiddel van de id’s kan ik deze html elementen benaderen en manipuleren in javascript. 
+Voor de knop heb ik in javascript een “click” eventlistener gedefinieerd die moet waarnemen als er op de knop wordt gedrukt. 
+
+Vervolgens heb ik een if-statement gemaakt met daarin ingebouwde javascript methods. Video.paused geeft een boolean aan.
+Dit geeft aan of de video gepauseerd is. Ja of nee.
+
+Is de video gepauseerd: ja
+dan gaat het knopje bij klikken video op play zetten.
+Is de video gepauseerd: nee
+Dan gaat het rechtstreeks naar de else en veranderd het de knop naar een pauze knop.
+*/
 
 //  testen van video contols
 const video = document.getElementById('video-1');
